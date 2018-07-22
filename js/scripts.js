65d8f51ef564f227f24d661ed2fcf66c22b25f95
Faiza Husain <faiza.ahsan1222@gmail.com>
2018-07-21 23:41:46 -0700

    modifies scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,37 @@
 
-// $(document).ready(function(){
-//   $("form#show").submit(function(){
-//     event.preventDefault();
-
-//     $("#img1").toggle();
-//     if($("#img1").css('display') == 'inline'){
-//       $("#img2").hide();
-//     } else {
-//       $("#img2").show();
-//     }
-//   });
-// });
-
-$(document).ready(function() {
-  $("form#show").submit(function(event) {
+$(document).ready(function(){
+  $("form#show").submit(function(){
     event.preventDefault();
 
-    //Handle 1st Image Render
-    if ($("#img1").css('display') == 'none' && $("#img2").css('display') == 'none') {
-      $("#img1").css('display', 'block');
+    $("#img1").toggle();
+    if($("#img1").css('display') == 'inline'){
+      $("#img2").hide();
+    } else {
+      $("#img2").show();
     }
-
-
-    //Toggle After 1st render
-    else if ($("#img1").css('display') == 'block') {
-      $("#img2").css('display', 'block');
-      $("#img1").css('display', 'none');
-    } else if ($("#img2").css('display') == 'block') {
-      $("#img1").css('display', 'block');
-      $("#img2").css('display', 'none');
-    }
-
-
   });
 });
+
+// $(document).ready(function() {
+//   $("form#show").submit(function(event) {
+//     event.preventDefault();
+
+//     //Handle 1st Image Render
+//     if ($("#img1").css('display') == 'none' && $("#img2").css('display') == 'none') {
+//       $("#img1").css('display', 'block');
+//     }
+
+
+//     //Toggle After 1st render
+//     else if ($("#img1").css('display') == 'block') {
+//       $("#img2").css('display', 'block');
+//       $("#img1").css('display', 'none');
+//     } else if ($("#img2").css('display') == 'block') {
+//       $("#img1").css('display', 'block');
+//       $("#img2").css('display', 'none');
+//     }
+
+
+//   });
+// });
 
