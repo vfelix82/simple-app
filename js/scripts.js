b9014c0d8233325aaa4d4d467c1166543f66c2ee
Victor Felix <vfelix82@gmail.com>
2018-07-22 09:41:25 -0700

    add working toggle js vanilla js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,32 @@
+let image1 = document.getElementById("img1");
+let image2 = document.getElementById("img2");
+let toggle = false;
 
-$(document).ready(function(){
-  $("form#show").submit(function(){
-    event.preventDefault();
+togglePictures = () => {
+  if (!toggle) {
+    image1.classList.add("hide");
+    image2.classList.remove("hide");
+  } else {
+    image1.classList.remove("hide");
+    image2.classList.add("hide");
+  }
+  toggle = !toggle;
+  console.log(toggle);
+};
 
-    $("#img1").toggle();
-    if($("#img1").css('display') == 'inline'){
-      $("#img2").hide();
-    } else {
-      $("#img2").show();
-    }
-  });
-});
+//
+// $(document).ready(function(){
+//   $("form#show").submit(function(){
+//     event.preventDefault();
+//
+//     $("#img1").toggle();
+//     if($("#img1").css('display') == 'inline'){
+//       $("#img2").hide();
+//     } else {
+//       $("#img2").show();
+//     }
+//   });
+// });
 
 // $(document).ready(function() {
 //   $("form#show").submit(function(event) {
@@ -21,7 +37,6 @@ $(document).ready(function(){
 //       $("#img1").css('display', 'block');
 //     }
 
-
 //     //Toggle After 1st render
 //     else if ($("#img1").css('display') == 'block') {
 //       $("#img2").css('display', 'block');
@@ -31,7 +46,5 @@ $(document).ready(function(){
 //       $("#img2").css('display', 'none');
 //     }
 
-
 //   });
 // });
-
